5cca223e0070e01ac5405dff840fd5c2d901ead3
Arkhin <nabillouh62110@gmail.com>
2022-06-13 13:07:29 +0200

    composant ListBiere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import ListBiere from './ListBiere'
+
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <ListBiere />
     </div>
   );
 }
---
+++ src/ListBiere.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class ListBiere extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            bieres:[],
+        }
+    }
+
+
+    componentDidMount() {
+        axios.get('https://api.punkapi.com/v2/beers').then(response => {
+            this.setState({bieres: response.data})
+            console.log(response.data)
+            console.log(this.state.bieres)
+        })
+
+    }
+
+
+    render() {
+
+
+
+        return (
+            <div>
+
+                <ul>
+
+                    {this.state.bieres.map((biere, id) =>
+    
+                        <li key={id}>
+                            <div>{biere.name}</div>
+                            <img src={biere.image_url} />
+                            
+                        </li>
+
+                    )}
+
+                </ul>
+               
+
+            </div>
+        )
+
+    }
+}
+
+
+export default ListBiere;
